018831b84a54156c8faa9e023510e4d16a728950
Moses-Yu-1 <dev@deltoid.kr>
2024-11-13 14:29:47 +0900

    fix layout of header

--- components/first-visit/aiForm.tsx
+++ components/first-visit/aiForm.tsx
@@ -28,6 +28,7 @@ import { Chat } from "./chat";
 import SendIcon from "@mui/icons-material/Send";
 import PrivacyConsentModal from "./PrivacyConsentModal";
 import { track } from "@vercel/analytics";
+import { Header } from "../header/header";
 // AI와 대화할 수 있는 form
 export const AiForm = () => {
   useEffect(() => {
@@ -112,111 +113,115 @@ export const AiForm = () => {
   };
   // console.log("AI Form");
   return (
-    <div className="flex flex-col w-full h-full pt-4">
-      <PrivacyConsentModal
-        open={open}
-        setOpen={setOpen}
-        handleAgree={handleAgree}
-        lang={lang}
-      ></PrivacyConsentModal>
-      <div className="max-w-[1400px] flex-1 overflow-hidden w-full rounded-xl md:rounded-3xl bg-white mt-2 pt-2 md:mt-3 shadow-lg px-3 xl:px-auto mx-auto">
-        <div className="w-full h-full pt-2 pb-4 md:pt-4 md:pb-8 flex flex-col max-w-[1200px] mx-auto">
-          <div className="flex-1 overflow-hidden px-3">
-            <div
-              ref={chatbgRef}
-              onScroll={() => autoScroll()}
-              className="flex flex-col gap-4 py-4 overflow-auto h-full"
-            >
-              {/* 대화 내용 / 기록 */}
-              {messages.map((chat) => (
-                <Chat
-                  key={generateRandomId(15)}
-                  role={chat.role}
-                  content={chat.content}
-                  createdAt={chat.createdAt}
-                />
-              ))}
+    <>
+      <Header />
+      <div className="flex flex-col w-full h-full pt-4">
+        <PrivacyConsentModal
+          open={open}
+          setOpen={setOpen}
+          handleAgree={handleAgree}
+          lang={lang}
+        ></PrivacyConsentModal>
+        <div className="max-w-[1400px] flex-1 overflow-hidden w-full rounded-xl md:rounded-3xl bg-white mt-2 pt-2 md:mt-3 shadow-lg px-3 xl:px-auto mx-auto">
+          <div className="w-full h-full pt-2 pb-4 md:pt-4 md:pb-8 flex flex-col max-w-[1200px] mx-auto">
+            <div className="flex-1 overflow-hidden px-3">
+              <div
+                ref={chatbgRef}
+                onScroll={() => autoScroll()}
+                className="flex flex-col gap-4 py-4 overflow-auto h-full"
+              >
+                {/* 대화 내용 / 기록 */}
+                {messages.map((chat) => (
+                  <Chat
+                    key={generateRandomId(15)}
+                    role={chat.role}
+                    content={chat.content}
+                    createdAt={chat.createdAt}
+                  />
+                ))}
+              </div>
+            </div>
+            {/* 입력창 */}
+            <div className="w-full px-1 md:px-10">
+              {isResult ? (
+                <div className="w-full flex items-center flex-row gap-2 md:gap-4">
+                  <div
+                    onClick={() => window.location.reload()}
+                    className="rounded-lg md:rounded-xl cursor-pointer text-center w-full font-[400] text-xs md:text-2xl shadow-md py-4 bg-[#D9D9D9]"
+                  >
+                    {LangContents[lang].refresh}
+                  </div>
+                  <div
+                    onClick={() => {
+                      track("Finish Chat");
+                      setOpen(true);
+                    }} // 모달창 열기
+                    className="bg-[#00387F] rounded-lg md:rounded-xl text-xs font-[400] md:text-2xl cursor-pointer shadow-md py-4 text-white text-center w-full"
+                  >
+                    {LangContents[lang].viewResult}
+                  </div>
+                </div>
+              ) : (
+                <form
+                  className="relative w-full"
+                  onSubmit={(e) => !isLoading && handleSubmit(e)}
+                  ref={formRef}
+                >
+                  <textarea
+                    className="w-full text-xs md:text-2xl font-[400] rounded-xl shadow-md pl-3 pr-8 py-4 md:pl-7 md:pr-16 md:py-6 outline-none row-auto resize-none"
+                    rows={1}
+                    // style="height: 80px;"
+                    style={{ height: "80px" }}
+                    ref={textareaRef}
+                    value={input}
+                    onChange={(e) => {
+                      if (e.target.value[e.target.value.length - 1] === "\n")
+                        return;
+                      handleInputChange(e);
+                      resizeTextarea();
+                    }}
+                    placeholder={
+                      isLoading
+                        ? LangContents[lang].genMedical
+                        : LangContents[lang].input
+                    }
+                    onKeyDown={(e) =>
+                      e.key === "Enter" &&
+                      !isLoading &&
+                      formRef.current?.requestSubmit()
+                    }
+                  ></textarea>
+                  <Image
+                    alt="send"
+                    loading="eager"
+                    width={40}
+                    height={40}
+                    decoding="async"
+                    className="absolute right-3 md:right-5 bottom-[22px] md:bottom-7 cursor-pointer w-4 h-4 md:w-10 md:h-10"
+                    src={Send}
+                    onClick={(e: FormEvent) =>
+                      handleSubmit(e as FormEvent<HTMLFormElement>)
+                    }
+                  />
+                  {isLoading && (
+                    <div className="absolute w-8 h-8 right-3 bottom-4 md:right-5 md:bottom-6 md:w-12 md:h-12">
+                      <PuffLoader
+                        loading={isLoading}
+                        size="100%"
+                        color="#9F9F9F"
+                      />
+                    </div>
+                  )}
+                </form>
+              )}
             </div>
           </div>
-          {/* 입력창 */}
-          <div className="w-full px-1 md:px-10">
-            {isResult ? (
-              <div className="w-full flex items-center flex-row gap-2 md:gap-4">
-                <div
-                  onClick={() => window.location.reload()}
-                  className="rounded-lg md:rounded-xl cursor-pointer text-center w-full font-[400] text-xs md:text-2xl shadow-md py-4 bg-[#D9D9D9]"
-                >
-                  {LangContents[lang].refresh}
-                </div>
-                <div
-                  onClick={() => {
-                    track("Finish Chat");
-                    setOpen(true)}} // 모달창 열기
-                  className="bg-[#00387F] rounded-lg md:rounded-xl text-xs font-[400] md:text-2xl cursor-pointer shadow-md py-4 text-white text-center w-full"
-                >
-                  {LangContents[lang].viewResult}
-                </div>
-              </div>
-            ) : (
-              <form
-                className="relative w-full"
-                onSubmit={(e) => !isLoading && handleSubmit(e)}
-                ref={formRef}
-              >
-                <textarea
-                  className="w-full text-xs md:text-2xl font-[400] rounded-xl shadow-md pl-3 pr-8 py-4 md:pl-7 md:pr-16 md:py-6 outline-none row-auto resize-none"
-                  rows={1}
-                  // style="height: 80px;"
-                  style={{ height: "80px" }}
-                  ref={textareaRef}
-                  value={input}
-                  onChange={(e) => {
-                    if (e.target.value[e.target.value.length - 1] === "\n")
-                      return;
-                    handleInputChange(e);
-                    resizeTextarea();
-                  }}
-                  placeholder={
-                    isLoading
-                      ? LangContents[lang].genMedical
-                      : LangContents[lang].input
-                  }
-                  onKeyDown={(e) =>
-                    e.key === "Enter" &&
-                    !isLoading &&
-                    formRef.current?.requestSubmit()
-                  }
-                ></textarea>
-                <Image
-                  alt="send"
-                  loading="eager"
-                  width={40}
-                  height={40}
-                  decoding="async"
-                  className="absolute right-3 md:right-5 bottom-[22px] md:bottom-7 cursor-pointer w-4 h-4 md:w-10 md:h-10"
-                  src={Send}
-                  onClick={(e: FormEvent) =>
-                    handleSubmit(e as FormEvent<HTMLFormElement>)
-                  }
-                />
-                {isLoading && (
-                  <div className="absolute w-8 h-8 right-3 bottom-4 md:right-5 md:bottom-6 md:w-12 md:h-12">
-                    <PuffLoader
-                      loading={isLoading}
-                      size="100%"
-                      color="#9F9F9F"
-                    />
-                  </div>
-                )}
-              </form>
-            )}
-          </div>
+        </div>
+
+        <div className="z-1 w-full text-xs md:text-xl text-[#777] font-[300] text-center mt-4">
+          ※정확한 진단은 내방하셔서 의사의 처방을 받으시길 바랍니다.
         </div>
       </div>
-
-      <div className="z-1 w-full text-xs md:text-xl text-[#777] font-[300] text-center mt-4">
-        ※정확한 진단은 내방하셔서 의사의 처방을 받으시길 바랍니다.
-      </div>
-    </div>
+    </>
   );
 };
